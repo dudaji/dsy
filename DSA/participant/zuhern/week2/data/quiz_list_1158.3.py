a13b67b5c228cd0c128fcd0f7c4767e5817ee2c6
zuhern <eyajiya@gmail.com>
2019-02-19 20:39:31 +0900

    링크드 리스트 백준 풀이 array로 구현

---
+++ DSA/participant/zuhern/week2/data/quiz_list_1158.3.py
@@ -0,0 +1,24 @@
+import sys
+
+cnt, gap = map(int, sys.stdin.readline().strip().split())
+ 
+tarList = []
+
+for i in range(1, cnt+1):
+  tarList.append(str(i))
+
+remove_index = gap-1
+result = '<'
+
+while not len(tarList) == 0:
+  result += str(tarList.pop(remove_index))
+  if not len(tarList) == 0:
+    result += ', '
+    remove_index += (gap-1)
+    # print("remove_index", remove_index, "len(tarList)", len(tarList))
+    if remove_index > len(tarList)-1 :
+      remove_index = remove_index % len(tarList)
+  else:
+    result += '>'
+ 
+sys.stdout.write(result)
